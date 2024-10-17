ba1a8a3e0767e0b0418b34ecf9394bfd49d4ca73
qianxia639 <2274000859@qq.com>
2024-10-17 15:43:17 +0800

    feat: 新增用户登录日志sql文件

--- db/migration/000001_add_user.up.sql
+++ db/migration/000001_add_user.up.sql
@@ -12,4 +12,28 @@ CREATE TABLE IF NOT EXISTS "users" (
     "password_changed_at" TIMESTAMPTZ NOT NULL DEFAULT '0001-01-01 00:00:00Z',
     "created_at" TIMESTAMPTZ NOT NULL,
     "updated_at" TIMESTAMPTZ NOT NULL
-)
+);
+
+COMMENT ON COLUMN "users"."id" IS '用户ID';
+
+COMMENT ON COLUMN "users"."username" IS '用户名';
+
+COMMENT ON COLUMN "users"."nickname" IS '用户昵称';
+
+COMMENT ON COLUMN "users"."password" IS '用户密码';
+
+COMMENT ON COLUMN "users"."salt" IS '随机盐';
+
+COMMENT ON COLUMN "users"."email" IS '用户邮箱';
+
+COMMENT ON COLUMN "users"."gender" IS '用户性别';
+
+COMMENT ON COLUMN "users"."avatar" IS '用户头像';
+
+COMMENT ON COLUMN "users"."is_online" IS '是否在线';
+
+COMMENT ON COLUMN "users"."password_changed_at" IS '上次密码更新时间';
+
+COMMENT ON COLUMN "users"."created_at" IS '创建时间';
+
+COMMENT ON COLUMN "users"."updated_at" IS '更新时间';
---
+++ db/migration/000002_add_login_log.up.sql
@@ -0,0 +1,23 @@
+-- 用户登录记录表
+CREATE TABLE IF NOT EXISTS "login_logs" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "user_id" BIGINT NOT NULL,
+    "login_time" TIMESTAMPTZ NOT NULL,
+    "ip"  VARCHAR(45) NOT NULL,
+    "ip_addr" VARCHAR(20) NOT NULL,
+    "is_login_exp" BOOLEAN NOT NULL DEFAULT FALSE,
+    "other" VARCHAR(16) NOT NULL DEFAULT '',
+    CONSTRAINT login_log_user_user_id_fk FOREIGN KEY ("user_id") REFERENCES "users" ("id")
+);
+
+COMMENT ON COLUMN "login_logs"."id" IS '日志ID';
+
+COMMENT ON COLUMN "login_logs"."user_id" IS '用户ID';
+
+COMMENT ON COLUMN "login_logs"."login_time" IS '登录时间';
+
+COMMENT ON COLUMN "login_logs"."ip" IS 'IP地址';
+
+COMMENT ON COLUMN "login_logs"."ip_addr" IS 'IP所属地';
+
+COMMENT ON COLUMN "login_logs"."is_login_exp" IS '是否登录异常';
